4c121927f4d75eb2ba425822a1b2e526bda2bb9e
CloudTestDrive <cloudtestdriveemea@gmail.com>
2018-12-18 09:26:03 +0100

    Create kubescript.sh

---
+++ kubescript.sh
@@ -0,0 +1,10 @@
+mkdir -p $HOME/.kube
+oci ce cluster create-kubeconfig --cluster-id <my_OKE_cluster_ocid> --file $HOME/.kube/config --region us-ashburn-1
+export KUBECONFIG=$HOME/.kube/config
+kubectl config view
+kubectl get nodes
+kubectl delete deployment nodejsmicro-k8s-deployment --ignore-not-found=true
+kubectl apply -f nodejs_micro.yaml
+kubectl get services nodejsmicro-k8s-service
+kubectl get pods
+kubectl describe pods
